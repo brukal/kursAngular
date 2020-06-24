68f269db83768e009fbc1bc224f07e2ab9ccbeb7
Bruno Kalanski <kalanskib@gmail.com>
2020-06-24 19:10:34 +0200

    sroda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'kurs';
+  loadedFeature='recipe';
+
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
 }
